85eecd83b6ab5b6157ba579f2eaab4bfe6b4f8c9
ElecAD <adelektrik3112@gmail.com>
2024-07-25 17:58:18 +0300

    Adding a drawer

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import router from "./router/router.ts";
+import VueSortable from "vue3-sortablejs";
+
 const app  = createApp(App);
 app.use(router)
+app.use(VueSortable);
 app.mount('#app')
