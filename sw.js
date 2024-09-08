e4ead79962ffeb6f0b99107019f9018f69a8f76c
ElecAD <adelektrik3112@gmail.com>
2024-09-08 22:25:35 +0300

    Edit cache map on application

--- sw.js
+++ sw.js
@@ -7,7 +7,42 @@ const MESSAGES = {
 const CACHE_KEY = "pwa-resources-cache"
 const CHANEL_KEY = "sw-chanel"
 
-const CACHE_MAP = ["/cards-manager/","/cards-manager/index.html","/cards-manager/manifest.json","/cards-manager/sw.js","/cards-manager/vite.svg","/cards-manager/zxing_reader.wasm","/cards-manager/resources/android-chrome-192x192.png","/cards-manager/resources/android-chrome-512x512.png","/cards-manager/resources/android-chrome-maskable-192x192.png","/cards-manager/resources/android-chrome-maskable-512x512.png","/cards-manager/resources/apple-touch-icon.png","/cards-manager/resources/favicon-32x32.png","/cards-manager/resources/safari-pinned-tab.svg","/cards-manager/logo/absolute.png","/cards-manager/logo/chitaj-gorod.png","/cards-manager/logo/detskij-mir.png","/cards-manager/logo/eldorado.png","/cards-manager/logo/evropa.png","/cards-manager/logo/funday.png","/cards-manager/logo/imperiya-sumok.png","/cards-manager/logo/kari.png","/cards-manager/logo/lenta.png","/cards-manager/logo/linia.png","/cards-manager/logo/magnit.png","/cards-manager/logo/not-found.png","/cards-manager/logo/ostin.png","/cards-manager/logo/planeta-zdorovya.png","/cards-manager/logo/poryadok.png","/cards-manager/logo/pyatyorochka.png","/cards-manager/logo/sportmaster.png","/cards-manager/logo/stalos.png","/cards-manager/logo/vite.png","/cards-manager/assets/index-CB8kUqrT.js","/cards-manager/assets/index-DTkWloqM.css"]
+const CACHE_MAP = ["/cards-manager/",
+    "/cards-manager/index.html",
+    "/cards-manager/manifest.json",
+    "/cards-manager/sw.js",
+    "/cards-manager/vite.svg",
+    "/cards-manager/zxing_reader.wasm",
+    "/cards-manager/resources/android-chrome-192x192.png",
+    "/cards-manager/resources/android-chrome-512x512.png",
+    "/cards-manager/resources/android-chrome-maskable-192x192.png",
+    "/cards-manager/resources/android-chrome-maskable-512x512.png",
+    "/cards-manager/resources/apple-touch-icon.png",
+    "/cards-manager/resources/favicon-32x32.png",
+    "/cards-manager/resources/safari-pinned-tab.svg",
+    "/cards-manager/public/logo/absolute.png",
+    "/cards-manager/public/logo/chitaj-gorod.png",
+    "/cards-manager/public/logo/detskij-mir.png",
+    "/cards-manager/public/logo/eldorado.png",
+    "/cards-manager/public/logo/evropa.png",
+    "/cards-manager/public/logo/funday.png",
+    "/cards-manager/public/logo/imperiya-sumok.png",
+    "/cards-manager/public/logo/kari.png",
+    "/cards-manager/public/logo/lenta.png",
+    "/cards-manager/public/logo/linia.png",
+    "/cards-manager/public/logo/magnit.png",
+    "/cards-manager/public/logo/not-found.png",
+    "/cards-manager/public/logo/ostin.png",
+    "/cards-manager/public/logo/planeta-zdorovya.png",
+    "/cards-manager/public/logo/poryadok.png",
+    "/cards-manager/public/logo/pyatyorochka.png",
+    "/cards-manager/public/logo/sportmaster.png",
+    "/cards-manager/public/logo/stalos.png",
+    "/cards-manager/public/logo/vite.png",
+    "/cards-manager/assets/index-CB8kUqrT.js",
+    "/cards-manager/assets/index-DTkWloqM.css"
+]
+
 const chanel = new BroadcastChannel(CHANEL_KEY)
 
 self.addEventListener('install', async (event) => {
